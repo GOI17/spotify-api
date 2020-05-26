47d0bde2cbcd40dc4101da2d7581ce3bd42299b9
GOI17 <chivas-gilbert@hotmail.com>
2020-05-26 03:01:33 -0700

    Actions registered on the container

--- app/container.php
+++ app/container.php
@@ -2,13 +2,20 @@
 
 declare(strict_types=1);
 
+use App\Application\Actions\ListAlbumsAction;
+use App\Application\Actions\ListBandsAction;
+use App\Application\Actions\ViewBandAlbumsAction;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Processor\UidProcessor;
+
 $container = $app->getContainer();
 
 $container['logger'] = function ($container) {
     $settings = $container->get('settings');
-    $logger = new Monolog\Logger($settings['logger']['name']);
-    $logger->pushProcessor(new Monolog\Processor\UidProcessor());
-    $logger->pushHandler(new Monolog\Handler\StreamHandler(
+    $logger = new Logger($settings['logger']['name']);
+    $logger->pushProcessor(new UidProcessor());
+    $logger->pushHandler(new StreamHandler(
         $settings['logger']['file'],
         $settings['logger']['level']
     ));
@@ -16,7 +23,14 @@ $container['logger'] = function ($container) {
     return $logger;
 };
 
+$container['ListBandsAction'] = function ($container) {
+    return new ListBandsAction($container->get('logger'));
+};
 
-$container[App\Application\Actions\Band\ListBandsAction::class] = function ($container) {
-    return new App\Application\Actions\Band\ListBandsAction($container->get('logger'));
+$container['ListAlbumsAction'] = function ($container) {
+    return new ListAlbumsAction($container->get('logger'));
+};
+
+$container['ViewBandAlbumsAction'] = function ($container) {
+    return new ViewBandAlbumsAction($container->get('logger'));
 };
