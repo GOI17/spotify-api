a7a32f85f3e262a816154cab0b1cea99eda4a758
GOI17 <chivas-gilbert@hotmail.com>
2020-05-25 20:39:40 -0700

    minor changes

--- app/settings.php
+++ app/settings.php
@@ -7,12 +7,11 @@ $settings = [];
 $settings['displayErrorDetails'] = true;
 $settings['determineRouteBeforeAppMiddleware'] = true;
 $settings['root'] = dirname(__DIR__);
-$settings['temp'] = $settings['root'] . '/tmp';
 $settings['public'] = $settings['root'] . '/public';
 $settings['logger'] = [
     'name' => 'spotify-api',
-    'file' => $settings['temp'] . '/logs/app.log',
-    'level' => \Monolog\Logger::ERROR
+    'file' => $settings['root'] . '/logs/app.log',
+    'level' => Monolog\Logger::DEBUG
 ];
 
 return $settings;
